0e47cd3c74b7b210e9c1a3746140be5beb210b55
hyc <976262957@qq.com>
2018-09-01 14:18:45 +0800

    hyc

--- hxphp/base/Model.php
+++ hxphp/base/Model.php
@@ -5,3 +5,25 @@
  * Date: 2018/8/31
  * Time: 18:35
  */
+namespace hxphp\base;
+
+use hxphp\db\Sql;
+
+class Model extends Sql{
+    protected $model;
+
+    public function __construct()
+    {
+        //获取数据库表明
+        if(!$this->table){
+            //获取模型名称
+            $this->model = get_class($this);
+
+            //删除类名最后的model字符
+            $this->model = substr($this->model, 0, -5);
+
+            //数据库表名与类名一致
+            $this->table = strtolower($this->model);
+        }
+    }
+}
